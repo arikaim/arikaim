389d45f8b7452fcfcdfa34e51a770b9ee85e8f29
arikaim <arikaim>
2019-11-08 08:43:58 +0200

    =

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
  * Arikaim
  *
  * @link        http://www.arikaim.com
- * @copyright   Copyright (c) 2017-2018 Konstantin Atanasov <info@arikaim.com>
- * @license     http://www.arikaim.com/license.html
+ * @copyright   Copyright (c) 2017-2019 Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
  * 
 */
 
