5d18eb2bc5a13ea9bd3d387ef6ab7b195bd9fee5
arikaim <arikaim>
2021-02-19 17:29:19 +0200

    =

---
+++ arikaim/config/arikaim-store.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+* Arikaim
+* @link        http://www.arikaim.com
+* @copyright   Copyright (c) 2017-2021 Konstantin Atanasov <info@arikaim.com>
+* @license     http://www.arikaim.com/license
+*/
+
+return [
+ 	'account' => [
+	],
+	'packages' => [
+	],
+	'product' => [
+	] 
+];
