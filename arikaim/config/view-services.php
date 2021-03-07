c1f55acc2503a98b4cd1a2ed3f874adb40a51474
arikaim <arikaim>
2021-03-07 12:28:45 +0200

    =

---
+++ arikaim/config/view-services.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+* Arikaim
+* @link        http://www.arikaim.com
+* @copyright   Copyright (c) 2017-2021 Konstantin Atanasov <info@arikaim.com>
+* @license     http://www.arikaim.com/license
+*/
+
+return [
+ 	'functions' => [
+	],
+	'filters' => [		
+	]
+];
